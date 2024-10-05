af9924ec6bedbb4e32876aab323dc37f6bfd1ee6
Mujtaba Shahbaz <mujtaba@Mujtabas-MacBook-Pro.local>
2024-10-05 20:37:51 +0500

    Another try

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('wss://smilecenterchatapp-llfaj.ondigitalocean.app')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
